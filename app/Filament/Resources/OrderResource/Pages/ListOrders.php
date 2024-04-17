613f0ee3e7ffc198801e433dc0626df9187064a3
tiomhs <tiomahesa0509@gmail.com>
2024-04-17 23:09:32 +0800

    Eps 8 || Filament Widget For Orders

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Resources\OrderResource\Pages;
 
-use App\Filament\Resources\OrderResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
+use App\Filament\Resources\OrderResource;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\OrderStatsResource\Widgets\OrderStats;
 
 class ListOrders extends ListRecords
 {
@@ -16,4 +18,25 @@ class ListOrders extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderStats::class
+        ];
+    }
+
+    public function getTabs(): array
+{
+    return [
+        null => Tab::make("All"), // Menggunakan argumen 'All'
+        'new' => Tab::make()->query(fn ($query) => $query->where('status','new')),
+        'processing' => Tab::make()->query(fn ($query) => $query->where('status','processing')),
+        'shipped' => Tab::make()->query(fn ($query) => $query->where('status','shipped')),
+        'delivered' => Tab::make()->query(fn ($query) => $query->where('status','delivered')),
+        'cancelled' => Tab::make()->query(fn ($query) => $query->where('status','cancelled')),
+    ];
+}
+
+
+    
 }
