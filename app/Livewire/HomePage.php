4dbac99d059827c665d4867d9b54ef5971a3ad11
Tio Mahesa <tiomahesa.work@gmail.com>
2024-05-14 09:24:02 +0800

    Eps 13 || Get data from database homepage and categoriespage

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class HomePage extends Component
 {
+    #[Title('Homepage - Mlanjow')]
     public function render()
     {
-        return view('livewire.home-page');
+        $brands = Brand::where('is_active', 1)->get();
+        $categories = Category::where('is_active', 1)->get();
+        return view('livewire.home-page',[
+            'brands' => $brands,
+            'categories' => $categories
+        ]);
     }
 }
